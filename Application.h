0affc14762d7749df9bb63599b588ff51ff57298
Harry <Harry@GLADOS>
2016-10-13 12:43:29 +0100

    Add project files.

---
+++ Application.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <windows.h>
+#include <d3d11_1.h>
+#include <d3dcompiler.h>
+#include <directxmath.h>
+#include <directxcolors.h>
+#include "resource.h"
+
+using namespace DirectX;
+
+struct SimpleVertex
+{
+    XMFLOAT3 Pos;
+    XMFLOAT4 Color;
+};
+
+struct ConstantBuffer
+{
+	XMMATRIX mWorld;
+	XMMATRIX mView;
+	XMMATRIX mProjection;
+};
+
+class Application
+{
+private:
+	HINSTANCE               _hInst;
+	HWND                    _hWnd;
+	D3D_DRIVER_TYPE         _driverType;
+	D3D_FEATURE_LEVEL       _featureLevel;
+	ID3D11Device*           _pd3dDevice;
+	ID3D11DeviceContext*    _pImmediateContext;
+	IDXGISwapChain*         _pSwapChain;
+	ID3D11RenderTargetView* _pRenderTargetView;
+	ID3D11VertexShader*     _pVertexShader;
+	ID3D11PixelShader*      _pPixelShader;
+	ID3D11InputLayout*      _pVertexLayout;
+	ID3D11Buffer*           _pVertexBuffer;
+	ID3D11Buffer*           _pIndexBuffer;
+	ID3D11Buffer*           _pVertexBufferPyr; //PYRAMID
+	ID3D11Buffer*           _pIndexBufferPyr; //PYRAMID
+	ID3D11Buffer*           _pConstantBuffer;
+
+	XMFLOAT4X4              _world;
+	XMFLOAT4X4              _planet1;
+	XMFLOAT4X4              _planet2;
+	XMFLOAT4X4              _moon1;
+	XMFLOAT4X4              _moon2;
+
+	XMFLOAT4X4              _view;
+	XMFLOAT4X4              _projection;
+	ID3D11DepthStencilView* _depthStencilView;
+	ID3D11Texture2D* _depthStencilBuffer;
+	ID3D11RasterizerState* _wireFrame;
+
+private:
+	HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow);
+	HRESULT InitDevice();
+	void Cleanup();
+	HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut);
+	HRESULT InitShadersAndInputLayout();
+	HRESULT InitVertexBuffer();
+	HRESULT InitIndexBuffer();
+	HRESULT InitIndexBufferPyramid();
+	HRESULT InitVertexBufferPyramid();
+
+	UINT _WindowHeight;
+	UINT _WindowWidth;
+
+public:
+	Application();
+	~Application();
+
+	HRESULT Initialise(HINSTANCE hInstance, int nCmdShow);
+
+	void Update();
+	void Draw();
+};
+
